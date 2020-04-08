5786b8dffec83d99f0fbfd2e2ef7f2d87987c9a7
m-mital <michal.mital@softwaremill.com>
2020-04-08 07:31:12 +0200

    Added inheritance approach

---
+++ src/main/scala/typeclassses/inheritance/HtmlWriter.scala
@@ -0,0 +1,15 @@
+package typeclassses.inheritance
+
+trait HtmlWriter {
+  def toHtml: String
+}
+
+case class Person(name: String, email: String) extends HtmlWriter {
+  def toHtml: String = s"<div>$name</div><div>$email</div>"
+}
+
+  object InheritanceApproach extends App {
+    val person = Person("John", "john@sml.com")
+    println(person.toHtml)
+  }
+
