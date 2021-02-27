95477356979d5c5242c5c881de5f04aec11864c3
m-mital <michal.mital@softwaremill.com>
2021-02-27 12:16:34 +0100

    Empty CovariantMaybe

--- src/main/scala/variance/CovariantMaybe.scala
+++ src/main/scala/variance/CovariantMaybe.scala
@@ -1,33 +1,11 @@
 package variance
 
-sealed trait CovariantMaybe[+A] {
-  def map[B](f: A => B): CovariantMaybe[B] = this match {
-    case Just(v) => Just(f(v))
-    case Nil => Nil
-  }
+// How to make a type covariant?
+sealed trait CovariantMaybe[A] {
+  def map[B](f: A => B): CovariantMaybe[B] = ???
 
-  def flatMap[B](f: A => CovariantMaybe[B]) = this match {
-    case Just(v) => f(v)
-    case Nil => Nil
-  }
-}
-
-final case class Just[A](v: A) extends CovariantMaybe[A] {
-  def isEmpty = false
-  def get = v
-}
-
-final case object Nil extends CovariantMaybe[Nothing] {
-  def isEmpty = true
-  def get = throw new NoSuchElementException("Nil.get")
+  def flatMap[B](f: A => CovariantMaybe[B]) = ???
 }
 
 object CovariantMaybeApp extends App {
-  val maybe: CovariantMaybe[Int] = Nil
-
-  val result = for {
-    i <- maybe
-  } yield i + 1
-
-  println(result)
 }
