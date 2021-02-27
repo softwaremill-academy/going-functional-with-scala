4c4b226336190426e9897a4c2b563ddbb8d8886d
m-mital <michal.mital@softwaremill.com>
2021-02-27 12:06:39 +0100

    Empty InvariantMaybe

--- src/main/scala/variance/InvariantMaybe.scala
+++ src/main/scala/variance/InvariantMaybe.scala
@@ -1,34 +1,14 @@
 package variance
 
 sealed trait InvariantMaybe[A] {
-  def map[B](f: A => B): InvariantMaybe[B] = this match {
-    case Just(v) => Just(f(v))
-    case Nil() => Nil[B]
-  }
-  def flatMap[B](f: A => InvariantMaybe[B]): InvariantMaybe[B] = this match {
-    case Just(v) => f(v)
-    case Nil() => Nil[B]
-  }
+  def map[B](f: A => B): InvariantMaybe[B] = ???
+
+  def flatMap[B](f: A => InvariantMaybe[B]): InvariantMaybe[B] = ???
 
   def isDefined: Boolean
+
   def get: A
 }
 
-final case class Just[A](value: A) extends InvariantMaybe[A] {
-  def isDefined: Boolean = true
-  def get = value
-}
-
-case class Nil[A]() extends InvariantMaybe[A] {
-  def isDefined = false
-
-  def get = throw new NoSuchElementException("Nil.get")
-}
-
 object InvariantMaybeApp extends App {
-  val maybe = Just(1)
-
-  for {
-    i <- maybe
-  } yield println(i)
 }
