3a4d986b3c9ee8cb5b823b78f8a141cc8d436457
m-mital <michal.mital@softwaremill.com>
2021-02-27 12:46:59 +0100

    Empty BoundedMaybe

--- src/main/scala/variance/BoundedMaybe.scala
+++ src/main/scala/variance/BoundedMaybe.scala
@@ -1,15 +1,6 @@
 package variance
 
 trait BoundedMaybe[+A] {
-  def isEmpty: Boolean
-  def map[B](f: A => B): BoundedMaybe[B] = ???
-  def flatMap[B](f: A => BoundedMaybe[B]): BoundedMaybe[B] = ???
-  def get: A
-
-  // covariant type A occurs in contravariant position in type => A of value default
-  def getOrElse[B >: A](default: B): B = {
-    if (isEmpty) default else get
-  }
 }
 
 final case class Just[A](value: A) extends BoundedMaybe[A] {
@@ -23,11 +14,4 @@ case object Nil extends BoundedMaybe[Nothing] {
 }
 
 object BoundedMaybeApp extends App {
-  val maybe = Just(3)
-
-  val result = for {
-    i <- maybe
-  } yield i + 10
-
-  println(s"Result: ${result}")
 }
